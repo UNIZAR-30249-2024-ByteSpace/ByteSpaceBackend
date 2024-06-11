8ba3481b3eb87775f2640b89f0d7f475e1a41cbf
Rael167 <75581099+Rael167@users.noreply.github.com>
2024-06-11 12:25:31 +0200

    Update PoliticaReserva.js

--- Domain/Value_Objects/PoliticaReserva.js
+++ Domain/Value_Objects/PoliticaReserva.js
@@ -4,8 +4,8 @@ class PoliticaReserva {
         if (usuario.rol === 'estudiante' && espacio.categoria !== 'salacomun') {
             return true;
         }
-        if ((usuario.rol === 'investigador contratado' || usuario.rol === 'docente investigador') &&
-        (usuario.departamento !== espacio.asignadoA && espacio.asignadoA !== 'EINA')) {
+        if ((usuario.rol === 'investigador contratado' || usuario.rol === 'docente-investigador') &&
+        (usuario.departamento !== espacio.asignadoA && espacio.asignadoA !== 'EINA' && (espacio.categoria === 'laboratorio' || espacio.categoria === 'seminario' || espacio.categoria === 'despacho'))) {
             return true;
         }
         if (usuario.rol === 'conserje' && espacio.categoria === 'despacho') {
@@ -13,7 +13,7 @@ class PoliticaReserva {
         }
         if (usuario.rol === 'técnico de laboratorio' &&
         ((espacio.categoria !== 'salacomun' && espacio.categoria !== 'laboratorio') ||
-        (usuario.departamento !== espacio.asignadoA && espacio.asignadoA !== 'EINA'))) {
+        (usuario.departamento !== espacio.asignadoA && espacio.asignadoA !== 'EINA' && espacio.categoria === 'laboratorio'))) {
             return true;
         }
         return false;
