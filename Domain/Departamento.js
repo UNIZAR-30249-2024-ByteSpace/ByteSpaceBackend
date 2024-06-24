19a90379589b11d773625569fb5ffd145a634573
JuanchoZGZ17 <756640@unizar.es>
2024-06-24 18:02:39 +0200

    Fix bug objeto valor

--- Domain/Departamento.js
+++ Domain/Departamento.js
@@ -1,9 +1,7 @@
 // ValueObject Departamento
 
 class Departamento {
-    constructor(nombre) {
-        //Debido a que la asignación puede ser no solo a departamentos sino a usuarios, se ha comentado la validación
-        /*
+    constructor(nombre) {        
         const validDepartments = [
             'informática e ingeniería de sistemas',
             'ingeniería electrónica y comunicaciones'
@@ -12,7 +10,7 @@ class Departamento {
         if (nombre && !validDepartments.includes(nombre)) {
             throw new Error(`Departamento inválido: ${nombre}`);
         }
-        */
+        
         this.nombre = nombre || null; // Permitir que el nombre sea nulo si no se proporciona
     }
 
